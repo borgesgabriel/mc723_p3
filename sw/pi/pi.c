78b4aea9230c69da42955edad73ebb94f262c0a0
Gabriel Borges <gabriel@gabrielborg.es>
2015-06-29 14:31:49 -0300

    Fix indentation

--- sw/pi/pi.c
+++ sw/pi/pi.c
@@ -18,66 +18,61 @@ int numThreads;                /* how many threads we use */
 /* the function a thread executes
  * Parameters: arg, a void* storing the address of the thread ID.
  */
-void *computePI(void *id)
-{
-    long double x,
-                width,
-                localSum = 0;
-    int i,
-        threadID = *((int*)id);
+ void *computePI(void *id) {
+  long double x, width, localSum = 0;
+  int i, threadID = *((int*)id);
 
-    width = 1.0 / intervals;
+  width = 1.0 / intervals;
 
-    for(i = threadID ; i < intervals; i += numThreads) {
-        x = (i+0.5) * width;
-        localSum += 4.0 / (1.0 + x*x);
-    }
+  for(i = threadID; i < intervals; i += numThreads) {
+    x = (i + 0.5) * width;
+    localSum += 4.0 / (1.0 + x*x);
+  }
 
-    localSum *= width;
+  localSum *= width;
 
-    pthread_mutex_lock(&piLock);
-    pi += localSum;
-    pthread_mutex_unlock(&piLock);
+  pthread_mutex_lock(&piLock);
+  pi += localSum;
+  pthread_mutex_unlock(&piLock);
 
-    return NULL;
+  return NULL;
 }
 
-int pi_main(int argc, char **argv)
-{
+int pi_main(int argc, char **argv) {
     pthread_t *threads;        /* dynarray of threads */
     void *retval;              /* unused; required for join() */
     int *threadID;             /* dynarray of thread id #s */
     int i;                     /* loop control variable */
 
-   if (argc == 3) {
-      intervals = atoi(argv[1]);
-      numThreads = atoi(argv[2]);
+ if (argc == 3) {
+  intervals = atoi(argv[1]);
+  numThreads = atoi(argv[2]);
 
-      if (numThreads < 1 || numThreads > MAXIMUM_NO_THREADS) {
-        printf("Invalid number of threads. Number of threads should"
-               " be between 1 and %d.\n", MAXIMUM_NO_THREADS);
-        return 0;
-      }
-
-      threads = malloc(numThreads*sizeof(pthread_t));
-      threadID = malloc(numThreads*sizeof(int));
-      pthread_mutex_init(&piLock, NULL);
-
-      for (i = 0; i < numThreads; i++) {
-        threadID[i] = i;
-        pthread_create(&threads[i], NULL, computePI, threadID+i);
-      }
-
-      for (i = 0; i < numThreads; i++) {
-         pthread_join(threads[i], &retval);
-      }
-      printf("Estimation of pi is %32.30Lf \n", pi);
-      printf("(actual pi value is 3.141592653589793238462643383279...)\n");
-    } else {
-      printf("Usage: ./a.out <numIntervals> <numThreads>");
-    }
-
+  if (numThreads < 1 || numThreads > MAXIMUM_NO_THREADS) {
+    printf("Invalid number of threads. Number of threads should"
+     " be between 1 and %d.\n", MAXIMUM_NO_THREADS);
     return 0;
+  }
+
+  threads = malloc(numThreads*sizeof(pthread_t));
+  threadID = malloc(numThreads*sizeof(int));
+  pthread_mutex_init(&piLock, NULL);
+
+  for (i = 0; i < numThreads; i++) {
+    threadID[i] = i;
+    pthread_create(&threads[i], NULL, computePI, threadID+i);
+  }
+
+  for (i = 0; i < numThreads; i++) {
+   pthread_join(threads[i], &retval);
+ }
+ printf("Estimation of pi is %32.30Lf \n", pi);
+ printf("(actual pi value is 3.141592653589793238462643383279...)\n");
+} else {
+  printf("Usage: ./a.out <numIntervals> <numThreads>");
+}
+
+return 0;
 }
 
 int main(int argc, char **argv) {
